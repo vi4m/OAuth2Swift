993339d4820a200d54f33ee6e4dc52da7d960155
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2016-06-12 14:01:51 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "OAuth2Middleware",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 7),
