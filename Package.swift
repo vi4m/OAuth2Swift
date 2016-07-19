9c5b52e5850e2be5a2f64729e7e3140a5c4881b8
Marcin Kliks <marcin.kliks@allegro.pl>
2016-07-19 12:45:59 +0200

    WIP

--- Package.swift
+++ Package.swift
@@ -4,9 +4,12 @@ let package = Package(
     name: "OAuth2Middleware",
     dependencies: [
 //        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 7),
+
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0), //, minor: 7),
+
 //        .Package(url: "https://github.com/VeniceX/File.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0, minor: 8),
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 9)
+
+        .Package(url: "https://github.com/VeniceX/HTTPSClient.git", majorVersion: 0), //, minor: 8),
+        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0) //, minor: 9)
     ]
 )
