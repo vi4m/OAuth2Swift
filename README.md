7e31f06995e821ad525f049b38101fa8ed3bb227
Marcin Kliks <marcin.kliks@allegrogroup.com>
2016-09-13 17:35:55 +0200

    Client credentials grant type

--- README.md
+++ README.md
@@ -15,11 +15,26 @@ See: [Zewo project](https://github.com/Zewo?utf8=✓&query=middleware) for more 
 Just add ```.Package(url: "https://github.com/vi4m/OAuth2Swift.git", majorVersion: 0, minor: 1)``` to your Package.swift file and run `swift build` to download dependency.
     
 # Usage
-    
+
+In this package, we user only server side OAuth2 with 2 options:
+- authorize using refresh tokens (RefreshTokenGrantType) 
+- authorize using client credentials (ClientCredentialsGrantType)
+
+In the first case, we will need to generate refresh token first, Google API Auth console allows to do it. Using
+this refresh token, and authentication information, we can generate next access tokens. 
+
+In the latter, we will ony need client_id and secret key. 
+
+Some basic URLs will be needed too.
+
+# Refresh Token Grant Type
+
 ```swift
 
-    middleware = OAuth2Middleware(clientId: clientId, clientSecret: clientSecret, 
-        refreshToken: refreshToken, tokenFileName: "/tmp/token")
+    let googleGrantType = RefreshTokenGrantType(clientId: "myapp", clientSecret: "secret", 
+                    refreshToken: "token", refreshTokenURL: "/oauth2/v3/token", baseURL: "https://www.googleapis.com:443")
+
+    middleware = OAuth2Middleware(grantType: googleGrantType, tokenFileName: "/tmp/token")
     
     // if token is not present(or expired) it will be redownloaded automatically using refresh_token
     client = try! Client(uri: "https://www.googleapis.com:443")
@@ -28,3 +43,14 @@ Just add ```.Package(url: "https://github.com/vi4m/OAuth2Swift.git", majorVersio
     var response = try client.get("/drive/v3/files", middleware: middleware)
     print(try response.body.becomeBuffer())
 ```
+
+# Client Credentials Grant Type
+
+```swift
+
+    let clientCredentialsGrantType = ClientCredentialsGrantType(clientId: "myapp", clientSecret: "secret", 
+                    authorizeTokenURL: "/auth/oauth/token", baseURL: "https://www.googleapis.com:443")
+    middleware = OAuth2Middleware(grantType: clientCredentialsGrantType, tokenFileName: "/tmp/token")
+```
+
+    
--- Sources/OAuth2Middleware.swift
+++ Sources/OAuth2Middleware.swift
@@ -2,6 +2,11 @@ import HTTP
 import File
 import HTTPSClient
 import JSON
+import Base64
+
+
+typealias GetNewTokenFunc = () -> String
+typealias GetNewCredentialsFunc = () -> String
 
 private var accessToken: String?
 
@@ -17,7 +22,7 @@ private func writeTokenToFile(token: String, filename: String) throws {
     }
 }
 
-private func readAccessTokenFromFile(filename: String) -> String? {
+private func readTokenFromFile(filename: String) -> String? {
     var file: File
     
     do {
@@ -36,42 +41,119 @@ private func readAccessTokenFromFile(filename: String) -> String? {
     return rez
 }
 
+enum OAuth2MiddlewareError: ErrorProtocol {
+    case TokenError
+}
 
-public struct OAuth2Middleware: Middleware {
-    var clientId, clientSecret, refreshToken : String
-    var tokenFileName: String
+
+public struct Token {
+    var value: String
+}
+
+public protocol GrantType {
+    func obtainToken() throws -> Token
+}
+
+public struct RefreshTokenGrantType: GrantType {
+    var clientId, clientSecret, baseURL, refreshTokenURL, refreshToken: String
     
-    public init(clientId: String, clientSecret: String, refreshToken: String, tokenFileName: String) {
+    public func obtainToken() throws -> Token {
+        do {
+            let client = try! Client(uri: "\(baseURL)")
+            let refreshTokenEncoded = try! refreshToken.percentEncoded(allowing: .uriPasswordAllowed)
+            let body = Data("client_id=\(self.clientId)&client_secret=\(self.clientSecret)&refresh_token=\(refreshTokenEncoded)&grant_type=refresh_token")
+            let headers: Headers = [
+                "Content-Type": "application/x-www-form-urlencoded"
+            ]
+            var response = try client.post(refreshTokenURL, headers: headers, body: body)
+            let buffer = try response.body.becomeBuffer()
+            let parsed = try! JSONParser().parse(data: buffer)
+            if response.statusCode != 200 {
+                print(parsed)
+                throw OAuth2MiddlewareError.TokenError
+            }
+            return Token(value: try parsed.asDictionary()["access_token"]!.asString())
+        } catch {
+            print("Response error")
+            throw OAuth2MiddlewareError.TokenError
+        }
+    }
+}
+
+public struct ClientCredentialsGrantType: GrantType {
+    var clientId, clientSecret, baseURL, authorizeTokenURL: String
+    var basicAuthCredentials: (String, String)?
+    
+    public init(clientId: String, clientSecret: String, baseURL: String, authorizeTokenURL: String, basicAuthCredentials: (String, String)? = nil ) {
         self.clientId = clientId
         self.clientSecret = clientSecret
-        self.refreshToken = refreshToken
+        self.baseURL = baseURL
+        self.authorizeTokenURL = authorizeTokenURL
+        self.basicAuthCredentials = basicAuthCredentials
+    }
+    
+    public func obtainToken() throws -> Token {
+        let client = try! Client(uri: "\(baseURL)")
+        let body = Data("client_id=\(self.clientId)&client_secret=\(self.clientSecret)&grant_type=client_credentials")
+        var headers: Headers = [
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
+        if let creds = basicAuthCredentials {
+            let credentials = Base64.encode("\(creds.0):\(creds.1)")
+            headers["Authorization"] = ["Basic \(credentials)"]
+        }
+        
+        do {
+            var response = try client.post(authorizeTokenURL,
+                                           headers: headers,
+                                           body: body)
+            let buffer = try response.body.becomeBuffer()
+            let parsed = try! JSONParser().parse(data: buffer)
+            if response.statusCode != 200 {
+                print(parsed)
+                throw OAuth2MiddlewareError.TokenError
+            }
+            return Token(value: try! parsed.asDictionary()["access_token"]!.asString())
+        } catch {
+            print("Response error")
+            throw OAuth2MiddlewareError.TokenError
+        }
+    }
+}
+
+
+public struct OAuth2Middleware: Middleware {
+    var tokenFileName: String
+    var grantType: GrantType
+    
+    public init(grantType: GrantType, tokenFileName: String) {
+        self.grantType = grantType
         self.tokenFileName = tokenFileName
         
-        if let token = readAccessTokenFromFile(filename: tokenFileName) {
+        if let token = readTokenFromFile(filename: tokenFileName) {
             accessToken = token
         }
     }
     
     func fetchTokenToCache() {
-        if let newToken = self.getNewToken() {
-            accessToken = newToken
-            try! writeTokenToFile(token: accessToken!, filename: tokenFileName)
-        }
-        else {
+        print("Fetching token to cache...")
+        guard let newToken = try? self.grantType.obtainToken() else {
             print("Failed to obtain new token")
             exit(1)
         }
+        accessToken = newToken.value
+        try! writeTokenToFile(token: accessToken!, filename: tokenFileName)
     }
     
     public func respond(to request: Request, chainingTo chain: Responder) throws -> Response {
         var request = request
         var result: Response
-      
+        
         /* If not have access token - authorize */
         if accessToken == nil {
             self.fetchTokenToCache()
         }
-        request.headers["Authorization"] = ["Bearer", "\(accessToken!)"]
+        request.headers["Authorization"] = ["Bearer \(accessToken!)"]
         result = try chain.respond(to: request)
         
         if result.statusCode == 401  {
@@ -84,28 +166,7 @@ public struct OAuth2Middleware: Middleware {
         return result
     }
     
-    func getNewToken() -> String? {
-        let client = try! Client(uri: "https://www.googleapis.com:443")
-        let refreshTokenEncoded = try! self.refreshToken.percentEncoded(allowing: .uriPasswordAllowed)
-        let body = Data("client_id=\(self.clientId)&client_secret=\(self.clientSecret)&refresh_token=\(refreshTokenEncoded)&grant_type=refresh_token")
-        let headers: Headers = [
-           "Content-Type": "application/x-www-form-urlencoded"
-        ]
-        
-        do {
-            var response = try client.post("/oauth2/v3/token", headers: headers, body: body)
-            let buffer = try response.body.becomeBuffer()
-            let parsed = try! JSONParser().parse(data: buffer)
-            if response.statusCode != 200 {
-               print(parsed)
-               return nil
-            }
-            return try! parsed.asDictionary()["access_token"]!.asString()
-        } catch {
-            print("Response error")
-            return nil
-        }
-    }
 }
+
 
 
