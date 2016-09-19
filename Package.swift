d4572e383b410d46119391db4f3803f126dcccd4
Marcin Kliks <marcin.kliks@allegrogroup.com>
2016-09-19 10:17:50 +0200

    Fixes.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "OAuth2Middleware",
     dependencies: [
-          .Package(url: "https://github.com/vi4m/Flux.git", majorVersion: 0, minor: 1),
+          .Package(url: "https://github.com/ZewoGraveyard/CEnvironment.git", majorVersion: 0, minor: 1),
+          .Package(url: "https://github.com/vi4m/Zewo.git", majorVersion: 0, minor: 8),
           .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 12)
     ]
 )
--- Sources/OAuth2Middleware.swift
+++ Sources/OAuth2Middleware.swift
@@ -1,3 +1,4 @@
+import Foundation
 import HTTP
 import File
 import HTTPClient
