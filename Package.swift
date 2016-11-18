ad4bf054836e27e640f4973da5ea91ca479e93fa
Marcin Kliks <marcin.kliks@icloud.com>
2016-11-18 16:12:32 +0100

    Upgrade zewo

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@ import PackageDescription
 let package = Package(
     name: "OAuth2Middleware",
     dependencies: [
-    .Package(url: "https://github.com/Zewo/Axis.git", majorVersion: 0, minor: 14),
-    .Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 14),
-    .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 14)
+    .Package(url: "https://github.com/vi4m/Zewo.git", majorVersion: 0, minor: 14)
     ]
 )
